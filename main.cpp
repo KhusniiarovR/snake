fbbb7a6a1d9ce4c3a574c3d7c36617eb9ec3e7bf
Ramir Khusniiarov <kr13193@auca.kg>
2024-10-07 12:56:27 +0600

    new way to delete tail

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@ using namespace std;
 int main() {
     int a,b;
     cin >> a >> b;
-    int delete_column =a-1;
-    int delete_row =b-1;
     int size[a][b];
     for (int i = 0; i < a; i++) {
         for (int j = 0; j < b; j++) {
@@ -23,27 +21,21 @@ int main() {
         cin >> move;
         switch (move) {
             case 'w':
-                delete_column = col;
-                delete_row = row;
                 col = col - 1;break;
             case 'a':
-                delete_row = row;
-                delete_column = col;
                 row = row - 1;break;
             case 's':
-                delete_column = col;
-                delete_row = row;
                 col = col + 1;break;
             case 'd':
-                delete_row = row;
-                delete_column = col;
                 row = row + 1;break;
             default:cout << move << endl;
         }
         for (int i = 0; i < a; i++) {
             for (int j = 0; j < b; j++) {
-                size[col][row] = 1;
-                size[delete_column][delete_row] = 0;
+                size[col][row] = 4;
+                if (size[i][j] != 0) {
+                    size[i][j] -= 1;
+                }
                 cout << size[i][j] << "\t";
             }
             cout << endl;
